fdf719682d3d3b12c2ce26869bb3eb7425c00928
DDD120 <dltnals9794@naver.com>
2024-01-16 16:56:30 +0900

    feat: step2 input validation

--- packages/client/src/components/step1/TimeInput.ts
+++ packages/client/src/components/step1/TimeInput.ts
@@ -16,11 +16,11 @@ export default class TimeInput extends Component<Props> {
       <div class="grid gap-6 mb-6 md:grid-cols-2">
         <div>
           <label for="start-time" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">시작 시각</label>
-          <input placeholder="HH:MM:SS" value="${startTime}" type="text" id="start-time" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg  focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"  required>
+          <input placeholder="HH:MM:SS" value="${startTime}" type="text" id="start-time" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg  focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required>
         </div>
         <div>
           <label for="end-time" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">종료 시각</label>
-          <input placeholder="HH:MM:SS" value="${endTime}" type="text" id="end-time" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg  focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"    required>
+          <input placeholder="HH:MM:SS" value="${endTime}" type="text" id="end-time" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg  focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required>
         </div>
       </div>
       <ul class="max-w-md my-4 space-y-1 text-sm text-gray-500 list-disc list-inside dark:text-gray-400">
--- packages/client/src/components/step2/Options.ts
+++ packages/client/src/components/step2/Options.ts
@@ -13,8 +13,11 @@ export default class Options extends Component<Props> {
     return `
       <h2 class="text-2xl font-bold dark:text-white mb-2">옵션</h2>
       <div class="mb-6">
-        <label for="resize" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">넓이 조절 (px 단위)</label>
-          <input value="${resizeWidth}" type="number" id="resize" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" />
+        <label for="resize" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">넓이</label>
+        <input value="${resizeWidth}"  min="100" max="1000" type="number" id="resize" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" />
+        <ul class="max-w-md my-4 space-y-1 text-sm text-gray-500 list-disc list-inside dark:text-gray-400">
+          <li>최소 넓이 100px, 최대 넓이 1000px 입니다.</li>
+        </ul>
       </div>
       <label for="speed" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">속도</label>
       <select id="speed" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
@@ -36,8 +39,16 @@ export default class Options extends Component<Props> {
   }
 
   handleWidthChange(e: Event) {
+    let value = (e.target as HTMLInputElement).value
+    if (Number(value) > 1000) {
+      value = '1000'
+    }
+    if (Number(value) < 100) {
+      value = '100'
+    }
+    ;(e.target as HTMLInputElement).value = value
     this.props.updateRef({
-      resizeWidth: (e.target as HTMLInputElement).value,
+      resizeWidth: value,
     })
   }
 
