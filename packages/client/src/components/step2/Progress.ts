0f8d4e27120143a669c357d3c80e720e998c03c0
DDD120 <dltnals9794@naver.com>
2024-01-16 16:59:11 +0900

    feat: update progress text

--- packages/client/src/components/step2/Progress.ts
+++ packages/client/src/components/step2/Progress.ts
@@ -5,7 +5,7 @@ export default class Progress extends Component {
     return `
       <div class="w-full">
         <div class="flex justify-between mb-1">
-          <span class="text-base font-medium text-blue-700 dark:text-white">Generating...</span>
+          <span class="text-base font-medium text-blue-700 dark:text-white">생성 중...</span>
           <span class="text text-sm font-medium text-blue-700 dark:text-white">0%</span>
         </div>
         <div class="w-full bg-gray-200 rounded-full h-2.5 dark:bg-gray-700">
