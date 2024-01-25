b57970b9236b480b2d69e8ca598dfb74faeb56e5
DDD120 <dltnals9794@naver.com>
2024-01-25 13:26:54 +0900

    style: language selector

--- packages/client/src/components/layout/LanguageSelector.ts
+++ packages/client/src/components/layout/LanguageSelector.ts
@@ -9,7 +9,7 @@ export default class LanguageSelector extends Component {
       <label for="language-selector" class="sr-only">${t(
         'langSelection'
       )}</label>
-      <select id="language-selector" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
+      <select id="language-selector" class="bg-gray-50 border-none text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700  dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
         <option value="ko" ${lang === 'ko' && 'selected'}>한국어</option>
         <option value="en" ${lang === 'en' && 'selected'}>English</option>
       </select>
